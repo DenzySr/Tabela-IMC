ca179c11690cfa2ae548cabf79914563e18ef643
Adrian Mira <adrian.mira@icloud.com>
2025-06-12 13:52:03 -0300

    arrumado o código para ficar visualmente mais agradável

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,15 +10,21 @@ const arrayIMC = [16,17,18.4,24.9,25,29.9,30,30.9,35,39.9,40];
 
 function enviarForm(evento){
     evento.preventDefault();
-    let resultado = vPeso.value / Math.pow(vAltura.value,2);
-    // return resultado
-    resultado = resultado.toFixed(1);
-    console.log(resultado)
+    const peso = Number(vPeso.value);
+    const altura = Number(vAltura.value);
+    const imc = getIMC(peso, altura);   
 
-    
-    
 
-    resultadoD.innerHTML = `<p class="txtResultado">O seu IMC é: ${resultado}, portanto, você está no estado de</p>`
+    resultadoD.innerHTML = `<p class="txtResultado">O seu IMC é: ${imc}, portanto, você está no estado de</p>`
+}
+
+function getIMC(peso, altura){
+    const imc = peso / Math.pow(altura,2);
+    return imc.toFixed(1);
+}
+
+function getEstadoImc (imc){
+
 }
 
 const botaoEnviar = document.querySelector('.btnEnviar')
